5c700206443e425e195797b18d5747df868a272c
emmanuel Dinga <kefundinga@yahoo.com>
2022-05-14 15:38:00 -0600

    new commit

---
+++ infra-cas.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.49"
+    }
+  }
+}
+
+provider "aws" {
+  region = us-east-1
+}
+
+
+
+
+module "rds-aurora" {
+  source                             = "git::git@github.SrDinga/Aurora-postgre-infra.git"
+  aws_rds_aurora_cluster_config      = var.aws_rds_aurora_cluster_config
+  aws_aurora_postgresql_cluster_tags = var.aws_aurora_postgresql_cluster_tags
+  environment                        = var.environment
+
+}
